1bae490eb1dd252744eaa06ab8877ef22f713af2
Sierlejek <sierlejek123@gmail.com>
2023-02-20 14:05:44 +0100

    Update Bazy

--- cukiernia.sql
+++ cukiernia.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 12 Lut 2023, 20:03
--- Wersja serwera: 10.4.27-MariaDB
--- Wersja PHP: 8.2.0
+-- Czas generowania: 20 Lut 2023, 13:59
+-- Wersja serwera: 10.4.24-MariaDB
+-- Wersja PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,7 +36,16 @@ CREATE TABLE `klienci` (
   `ulica` varchar(225) NOT NULL,
   `nr_domu` int(11) NOT NULL,
   `czy_stały` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `klienci`
+--
+
+INSERT INTO `klienci` (`id`, `imie`, `nazwisko`, `num_tel`, `miasto`, `ulica`, `nr_domu`, `czy_stały`) VALUES
+(1, 'Kamil', 'Ślimak', 123456789, 'Garzyn', 'Kowalska', 10, 1),
+(2, 'Michał', 'Ślimak', 987654321, 'Leszno', '3 Maja', 69, 0),
+(3, 'Kamil', 'Pierwiastek', 576682359, 'Lipno', 'Jana Pawła 2', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -52,7 +61,16 @@ CREATE TABLE `pracownicy` (
   `num_tel` int(11) NOT NULL,
   `miasto` varchar(225) NOT NULL,
   `wypłata` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `pracownicy`
+--
+
+INSERT INTO `pracownicy` (`id`, `imie`, `nazwisko`, `posada`, `num_tel`, `miasto`, `wypłata`) VALUES
+(1, 'Michał', 'Adam', 'Kierownik', 234645785, 'Krzywiń', 1240),
+(2, 'Janusz', 'Tracz', 'Menadżer', 696969420, 'Poznań', 43900),
+(3, 'Jan', 'Kowalski', 'Pracownik', 11119, 'Janiszewo', 100);
 
 -- --------------------------------------------------------
 
@@ -64,10 +82,18 @@ CREATE TABLE `produkty` (
   `id` int(11) NOT NULL,
   `nazwa` varchar(225) NOT NULL,
   `rodzaj` int(11) NOT NULL,
-  `cena` int(11) NOT NULL,
+  `cena` float NOT NULL,
   `waga` int(11) NOT NULL,
   `obraz` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `produkty`
+--
+
+INSERT INTO `produkty` (`id`, `nazwa`, `rodzaj`, `cena`, `waga`, `obraz`) VALUES
+(5, 'Draże Korsarze', 5, 2.59, 120, ''),
+(6, 'Magdalenka', 4, 1.2, 20, '');
 
 -- --------------------------------------------------------
 
@@ -79,7 +105,7 @@ CREATE TABLE `rodzaje` (
   `id` int(11) NOT NULL,
   `rodzaj` varchar(255) NOT NULL,
   `czy_na_wynos` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `rodzaje`
@@ -100,9 +126,17 @@ INSERT INTO `rodzaje` (`id`, `rodzaj`, `czy_na_wynos`) VALUES
 
 CREATE TABLE `zamówienia` (
   `id` int(11) NOT NULL,
-  `zawartość` text NOT NULL,
-  `cena` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `zawartość` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `zamówienia`
+--
+
+INSERT INTO `zamówienia` (`id`, `zawartość`) VALUES
+(1, '2 Magdalenki, 3 Draże Korsarze, 1 Orzechowa historia'),
+(2, '1 Orzechowa historia'),
+(3, '2 Magdalenki');
 
 -- --------------------------------------------------------
 
@@ -116,8 +150,17 @@ CREATE TABLE `zamówienia_info` (
   `id_zamówienia` int(11) NOT NULL,
   `data_zamówienia` date NOT NULL,
   `data_wykonania` date DEFAULT NULL,
-  `łączna_cena` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `cena_dostawy` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `zamówienia_info`
+--
+
+INSERT INTO `zamówienia_info` (`id`, `id_klienta`, `id_zamówienia`, `data_zamówienia`, `data_wykonania`, `cena_dostawy`) VALUES
+(1, 1, 3, '2023-02-14', '2023-02-14', 2.5),
+(2, 2, 1, '2023-02-14', '2023-02-16', 2.5),
+(3, 3, 2, '2023-01-10', '2023-02-18', 2.5);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -170,19 +213,19 @@ ALTER TABLE `zamówienia_info`
 -- AUTO_INCREMENT dla tabeli `klienci`
 --
 ALTER TABLE `klienci`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT dla tabeli `pracownicy`
 --
 ALTER TABLE `pracownicy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT dla tabeli `produkty`
 --
 ALTER TABLE `produkty`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT dla tabeli `rodzaje`
@@ -194,13 +237,13 @@ ALTER TABLE `rodzaje`
 -- AUTO_INCREMENT dla tabeli `zamówienia`
 --
 ALTER TABLE `zamówienia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT dla tabeli `zamówienia_info`
 --
 ALTER TABLE `zamówienia_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ograniczenia dla zrzutów tabel
